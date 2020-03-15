aa66ea41549e3acb24b7da071bc29962ac5dec27
Kevin G. Van Slyke <kgvansly@buffalo.edu>
2020-03-15 17:38:00 -0400

    Create keras_classification.py

---
+++ keras_classification.py
@@ -0,0 +1,16 @@
+from keras.datasets import mnist
+
+# read the data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+def classification_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(num_pixels, activation='relu', input_shape=(num_pixels,)))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    
+    
+    # compile model
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
